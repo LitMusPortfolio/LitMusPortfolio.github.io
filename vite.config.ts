3db898510995d00a0081907424b1e082653106ee
u1-mas <u1-mas>
2025-06-01 02:56:13 +0900

    feat: パスエイリアス(@)を設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
@@ -6,4 +7,9 @@ export default defineConfig({
   plugins: [react()],
   // GitHub Pages用のベースパス設定
   base: process.env.NODE_ENV === "production" ? "/" : "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
